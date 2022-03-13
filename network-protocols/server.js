4f46aeac3cb9591722dcd379fd7fc4f4e9347349
Uday <budayteja009@gmail.com>
2022-03-13 23:22:03 +0530

    First commit

---
+++ network-protocols/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/hello', (req, res) => {
+    console.log('Headers', req.headers);
+    console.log('Method', req.method);
+    console.log('Body', req.body);
+    res.send('Received GET request:\n');
+});
+
+app.post('/hello', (req, res) => {
+    console.log('Headers', req.headers);
+    console.log('Method', req.method);
+    console.log('Body', req.body);
+    res.send('Received POST request:\n');
+});
---
+++ storage/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs = require('fs');
+
+const DATA_DIR = 'db_data';
+
+const app = express();
+app.use(express.json());
+
+const hashTable = {};
+
+app.post('/memory/:key', (req, res) => {
+    hashTable[req.params.key] = req.body;
+    res.send();
+});
+
+app.get('/memory/:key', (req, res) => {
+    const key = req.params.key;
+    if(key in hashTable) {
+       return res.send(hashTable[key]);
+    }
+});
+
+app.post('/disk/:key', (req, res) => {
+    const destinationFile = `${DATA_DIR}/${req.params.key}`;
+    fs.writeFileSync(destinationFile, req.body.data);
+    res.send();
+});
+
+app.get('/disk/:key', (req, res) => {
+    const destinationFile = `${DATA_DIR}/${req.params.key}`;
+    try {
+        const data = fs.readFileSync(destinationFile);
+        res.send(data);
+    } catch (e) {
+        return res.send('null');
+    }
+});
+
+app.listen(3001, () => {
+    console.log(`Listening on port 3001`);
+});
